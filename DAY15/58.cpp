f2a48d325aa41cfd6e847c580a52a64923392382
unknown <aryanmishra01256@gmail.com>
2026-06-15 21:31:40 +0530

    DAY 15 Uploaded

---
+++ DAY15/58.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int temp = arr[0];
+
+    for(int i = 0; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    arr[n - 1] = temp;
+
+    cout << "Array after left rotation: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
