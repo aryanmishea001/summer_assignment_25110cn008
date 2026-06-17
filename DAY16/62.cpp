27be252e573ca97a3de3a8c6dce2e2ad28461e80
unknown <aryanmishra01256@gmail.com>
2026-06-17 08:47:31 +0530

    DAY 16 uploaded

---
+++ DAY16/62.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int maxFreq = 0, element;
+
+    for(int i = 0; i < n; i++) {
+        int count = 0;
+
+        for(int j = 0; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+            }
+        }
+
+        if(count > maxFreq) {
+            maxFreq = count;
+            element = arr[i];
+        }
+    }
+
+    cout << "Element with Maximum Frequency = " << element;
+    cout << "\nFrequency = " << maxFreq;
+
+    return 0;
+}
